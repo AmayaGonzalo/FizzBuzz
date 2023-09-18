d9476b1cbeea394d73780ef3c7aef27a58cb8882
Gonzalo <gonzaloamaya@tdf.edu.ar>
2023-09-18 16:52:44 -0300

    pruebas unitaria controller FizzBuzz

--- src/fizzbuzz/fizzbuzz.controller.spec.ts
+++ src/fizzbuzz/fizzbuzz.controller.spec.ts
@@ -4,15 +4,50 @@ import { FizzbuzzService } from './fizzbuzz.service';
 
 describe('FizzbuzzController', () => {
   let controller: FizzbuzzController;
+  let service: FizzbuzzService;
+
+  //Para mock all
+  let mockedFizzBuzzValue = 'Buzz';
+  let mockedFizzBuzzService = {
+    fizzbuzz: ()=> mockedFizzBuzzValue
+  }
+  //
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FizzbuzzController],
       providers: [FizzbuzzService],
-    }).compile();
+    })
+    //para mock all
+    .overrideProvider(FizzbuzzService)    
+    .useValue(mockedFizzBuzzService)
+    //
+    .compile();
 
     controller = module.get<FizzbuzzController>(FizzbuzzController);
+    service = module.get<FizzbuzzService>(FizzbuzzService);
   });
+
+  // test('al poner 3 me devuelve Fizz con spyON', ()=>{
+  //   const result = 'Fizz';
+  //   jest.spyOn(service, 'fizzbuzz').mockImplementation(()=>result);
+
+  //   expect(controller.fizzbuzz(3)).toBe('Fizz');
+  // })
+
+  test('mock all al poner 5 me devuelve Buzz con spyON',()=>{
+    expect(controller.fizzbuzz(5)).toBe(mockedFizzBuzzValue);
+  })
+
+  test('mock all al poner 3 me devuelve Fizz con spyON',()=>{
+    mockedFizzBuzzValue = 'Fizz';
+    expect(controller.fizzbuzz(3)).toBe(mockedFizzBuzzValue);
+  })
+
+  test('mock all al poner 1 me devuelve el 1 con spyON',()=>{  
+    mockedFizzBuzzValue = '1';  
+    expect(controller.fizzbuzz(1)).toBe(mockedFizzBuzzValue);
+  })
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
